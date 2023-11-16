ddd825e62a5ae6e57a5774aac42eb8928dbd9b45
Ágoston Babicz <97826931+AgostonBabicz@users.noreply.github.com>
2023-11-16 15:50:56 +0100

    Touch up

--- EfcDataAccess/ForumContext.cs
+++ EfcDataAccess/ForumContext.cs
@@ -11,7 +11,7 @@ public class ForumContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-       optionsBuilder.UseSqlite(@"Data Source = /Users/AgostonBabicz/Documents/GitHub/DNP_Assignment/EfcDataAccess/DB.db");
+       //optionsBuilder.UseSqlite(@"Data Source = /Users/AgostonBabicz/Documents/GitHub/DNP_Assignment/EfcDataAccess/DB.db");
        // This is an absolute path for the DB. Please change it in order to make the application work. The relative path didn't work for some reason.
     }
 
